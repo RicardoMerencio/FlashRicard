592ac50f1bd37e2c886e2ff9f3ade2225a3f5257
RicMerencio <169045596+RicardoMerencio@users.noreply.github.com>
2024-11-29 15:19:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'League of Legends',
+    'Qual a região de origem da Ambessa Medarda?',
+    'Noxus'   
+)
+
+criaCartao(
+    'Arcane',
+    'Quem era o mago que salvou Jayce e sua mãe quando ele ainda era criança?',
+    'O Viktor do futuro, depois de perceber que a perfeição era solitária, entregou a runa ao Jayce criança, pois só ele conseguiria mudar esse futuro.'
+)
+
+criaCartao(
+    'Zaun',
+    'Por que a Jinx é tão ágil?',
+    'Por conta do cintíla que foi injetado nela para que ela não morresse, assim transformando ela numa espécie de super-soldado.   '
+)
+
+criaCartao(
+    'Arcane',
+    'Por que na realidade alternativa as coisas foram diferentes?',
+    'Por conta que la a Hextec não existe, com a morte da Vi, possivelmente Jayce foi preso ou impossibilitado de continuar estudando as runas.'
+)
